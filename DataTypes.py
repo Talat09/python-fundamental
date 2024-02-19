8f44448654465f0ea20f8656c7ed6cf7305584f4
Talat09 <talat181008312@gmail.com>
2024-02-19 18:51:39 +0600

    done data types

---
+++ DataTypes.py
@@ -0,0 +1,15 @@
+# string "dihan";
+# integer  1/2/3;
+# float  20.56;
+# boolean TRUE/False;
+# None;
+name="Talat Mahmud."
+age=18
+price=25.99
+old=False
+dance=None
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(dance))
