887b2e65ef41f79a9b1e52b0e14ee42f91db00db
Talat09 <talat181008312@gmail.com>
2024-02-21 19:16:12 +0600

    done input

---
+++ input.py
@@ -0,0 +1,14 @@
+#input() statement is used to accept values (using keyboard) from user
+
+#String input
+name=input("Enter your name: ")
+
+#int input
+age=int(input("Enter your age: "))
+
+#float input
+price=float(input("Enter your price: "))
+
+
+#all print
+print("My Name is:", name, "My Age is:", age, "My price is", price)
