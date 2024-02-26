289ed11c6890987caa24c58595c6ee341fb3951a
Talat09 <talat181008312@gmail.com>
2024-02-27 02:19:30 +0600

    done arithmetic operators

---
+++ ArithmeticOperator.py
@@ -0,0 +1,14 @@
+#Arithmetic Operators
+a= 5
+b= 2
+add= a+b
+sub= a-b
+mul= a*b
+div= a/b
+#module or reminder operator
+mod= a%b
+#power operator
+pow= a**b
+#floor or integer division
+floor= a//b
+print(add,"\n",sub,"\n",mul,"\n",div,"\n",mod,"\n",pow,"\n",floor)
