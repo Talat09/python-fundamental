28e5ce6230ed46575bdbdc5f9f837df6f7c2330e
Talat09 <talat181008312@gmail.com>
2024-03-06 21:26:05 +0600

    start loops

---
+++ Chapter-5 Loops/Loops.py
@@ -0,0 +1,26 @@
+#Loops in python
+#Loops are used to repeat instruction
+#There are two types of loops while and for loops
+
+#while loops
+"""
+while condition:
+    some work
+"""
+#write a program to print Hello world 5 times
+i=0
+while i<5:
+    print("Hello world")
+    i = i+1
+    print(i)
+#write a program to print Talat Mahmud 4 times
+j=1
+while j<=5:
+    print("Talat Mahmud")
+    j+=1
+
+#print number from 1 to 100
+x=1
+while x<=100:
+    print(x)
+    x+=1
