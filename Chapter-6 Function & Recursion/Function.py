18ff12619886257ebfdd44c038d54a69b2bed8f3
Talat09 <talat181008312@gmail.com>
2024-03-10 12:09:10 +0600

    done fundamental of functions

---
+++ Chapter-6 Function & Recursion/Function.py
@@ -0,0 +1,31 @@
+#Function in python
+#block of statements that perform a specific task
+"""
+function definition
+def function_name(param1,param2):
+    #statements
+    #statements
+    #statements
+    return value
+function_name(argument1,argument2)
+"""
+#write a program adding two numbers using function
+def add(a,b):
+    total=a+b
+    return total
+sum=add(10,20)
+print(sum)
+#write a program to print Hello world 5 times
+def print_hello():
+        print("Hello world")
+     
+print(print_hello())#if function not return anything then its none
+#average of 3 numbers
+def average(a,b,c):
+    total=a+b+c
+    average=total/3
+    return average
+
+output=average(14,5,45)
+print(output)
+
