d296e2742a82b92fa7908f27e06f58a088d27809
Talat09 <talat181008312@gmail.com>
2024-03-11 23:55:02 +0600

    done types of function and default parameter

---
+++ Chapter-6 Function & Recursion/Practise1.py
@@ -0,0 +1,57 @@
+#1. Write a program to print the length of a list(list is the parameter)
+world_cities=["Amman","Irbid","Jerash","Cairo","London"]
+bd_cities=["Dhaka","Chittagong","Khulna","Barishal","Rangpur"]
+
+def print_cities(list1,list2):
+   sum_cities=list1+list2
+   return len(sum_cities)
+    # print("Length of list:",length)
+
+output=print_cities(world_cities,bd_cities)
+print(output)
+#2. write a program to print the element of a list in a single line.(list is the parameter)
+print(world_cities[0],end=" ")
+print(world_cities[1],end="\n")
+
+def print_items(list):
+    for i in list:
+        print(i,end="\n")
+    return i
+
+print(print_items(world_cities))
+print(print_items(bd_cities))
+
+#3. write a program to find the factorial of n.
+
+"""
+n=5
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(fact)
+
+
+//
+def print_factorial(n):
+    fact = 1
+    i = 1
+    while i <= n:
+        fact *= i
+        i += 1
+    return fact
+
+print(print_factorial(5))
+"""
+def print_factorial(n):
+    fact=1
+    for i in range(1,n+1):
+        fact=fact*i
+    return fact #return the value of the factorial
+print(print_factorial(5))
+
+#write a program convert USD to BDT
+def convert_usd_to_bdt(usd):
+    bdt=usd*110
+    return bdt
+
+print(convert_usd_to_bdt(100),"BDT")
---
+++ Chapter-6 Function & Recursion/TypesOfFunction.py
@@ -0,0 +1,26 @@
+#Types of Functions
+#1. Built-in Functions
+"""
+print()
+len()
+type()
+range()
+"""
+print("Hello World","Talat Mahmud")#separator=" "
+print("Hello World")#separator=":"
+print("Talat Mahmud")#end="\n"
+print("AMIR", end=" ")
+print("HAMJA")
+#2. User-defined Functions
+"""
+which program write by user
+"""
+
+#Default parameter->Assigning a default value to parameter,which is used when no argument is passed.
+#default arguments always must be at the end of the parameter list.(b=1)
+#calculate multiplication
+def multiply(a,b=1):
+    print(a*b)
+    return a*b
+
+multiply(2)
